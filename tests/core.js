d9da2517bc2d15d72959318f145dbd4c36da4df6
Minjun <84955607+dev-yuka05@users.noreply.github.com>
2022-06-28 21:39:45 +0900

    Feat : Object type new style rule constructor

--- tests/core.js
+++ tests/core.js
@@ -6,27 +6,41 @@
 */
 
 (function () {
+    
+       // Create a style element if there is no style element and add a style rule
     const createStyle=(selector,style)=>{
-       if(!document.querySelector('head style')){
-           let createStyleElement = document.createElement('style');
-           document.querySelector('head').appendChild(createStyleElement);
-           createStyleElement.sheet.insertRule(`${selector}{${style}}`);
-       }else{
-           document.querySelector('head style').sheet.insertRule(`${selector}{${style}}`);
-       }
-   };
-//    const createStyleSelectorArray=(selector,style)=>{
-//        selector.forEach(element => {
-//            createStyle(element,style);
-//        });
-//    };
+        if(!document.querySelector('head style')){
+            let createStyleElement = document.createElement('style');
+            document.querySelector('head').appendChild(createStyleElement);
+            createStyleElement.sheet.insertRule(`${selector}{${style}}`);
+        }else{
+            document.querySelector('head style').sheet.insertRule(`${selector}{${style}}`);
+        }
+    };
 
-   // createStyleArrayClass([{className: "line-bottom", styleValue: "underline"}], 'text-decoration')
-   const createStyleArrayClass=(selector,styleName)=>{
-       selector.forEach(ele => {
-           createStyle(`.${ele.className}`,`${styleName}:${ele.styleValue};`);
-       });
-   };
-   
+    // When a style value becomes a class directly
+    const createStyleArrayClass=(selector,style,firstName)=>{
+        if(firstName){
+            selector.forEach(element => {
+                createStyle(`.${firstName}-${element}`,`${style}:${element};`)
+            });
+        }else{
+            selector.forEach(element => {
+                createStyle(`.${element}`,`${style}:${element};`)
+            });
+        }
+    };
+
+    const newCreateStyle=(selector,style,firstName)=>{
+        if(firstName){
+            selector.forEach(element => {
+                createStyle(`.${firstName}-${element.name}`,`${style}:${element.value};`);
+            });
+        }else{
+            selector.forEach(element => {
+                createStyle(`.${element.name}`,`${style}:${element.value};`);
+            });
+        }
+    };
    
 })();
